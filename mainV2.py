d557834656f203583cf3477a0196148d3486bb24
k4my4r <56694872+k4my4r@users.noreply.github.com>
2021-05-28 21:05:21 +0200

    Update mainV2.py

--- mainV2.py
+++ mainV2.py
@@ -1,6 +1,6 @@
 import moduleV2
 
-inputs = input('/ecg')
+inputs = input('/ecg.xlsx')
 for i in inputs.split(", "):
     print('it\'s "{}" turn to be analysed'.format(i))
     unana = moduleV2.Stats(i, '{}_{}'.format(i, 'output'))
